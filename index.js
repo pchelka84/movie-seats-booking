97a9022521cfc4b0a28f470d35cd0ba46a7dd2f9
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-07-31 22:00:26 -0400

    Change total price on movie change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 //Update total and count
 function updateSelectedCount() {
@@ -16,6 +16,13 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+// Seat click event
 container.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("seat") &&
